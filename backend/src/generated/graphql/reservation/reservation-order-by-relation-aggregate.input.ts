e743fd410deaf6c0a860a7910cbe821451f75352
mcartage32 <mcartage33@gmail.com>
2026-04-26 01:11:58 -0500

    Se elimina libreria prisma-nestjs-graphq

--- backend/src/generated/graphql/reservation/reservation-order-by-relation-aggregate.input.ts
+++
@@ -1,9 +0,0 @@
-import { Field } from '@nestjs/graphql';
-import { InputType } from '@nestjs/graphql';
-import { SortOrder } from '../prisma/sort-order.enum.js';
-
-@InputType()
-export class ReservationOrderByRelationAggregateInput {
-  @Field(() => SortOrder, { nullable: true })
-  _count?: `${SortOrder}`;
-}
